8ae1ddcd8205a55172e707e9ec9cef768526848d
Thomas Guillem <thomas@gllm.fr>
2018-05-03 15:09:52 +0200

    squash: [RFC] Add clock.h header

--- src/clock/clock.h
+++ src/clock/clock.h
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
-typedef stuct vlc_clock_master_t vlc_clock_master_t;
+typedef struct vlc_clock_master_t vlc_clock_master_t;
 typedef struct vlc_clock_t vlc_clock_t;
 
 /**
