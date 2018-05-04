eaa9afbf16169450b7910cd7a282dc4079c8cda7
Rémi Denis-Courmont <remi@remlab.net>
2018-05-04 19:08:27 +0300

    aout: use vlc_clock_t
    Signed-off-by: Thomas Guillem <thomas@gllm.fr>

--- src/input/decoder.c
+++ src/input/decoder.c
@@ -375,7 +375,7 @@ static int aout_update_format( decoder_t *p_dec )
             if( p_dec->fmt_out.i_codec == VLC_CODEC_DTS )
                 var_SetBool( p_aout, "dtshd", p_dec->fmt_out.i_profile > 0 );
 
-            if( aout_DecNew( p_aout, &format,
+            if( aout_DecNew( p_aout, &format, p_owner->p_clock,
                              &p_dec->fmt_out.audio_replay_gain,
                              &request_vout ) )
             {
