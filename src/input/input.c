86f9af500a13465fafb6cfaf2c6da9b209940bdf
Steve Lhomme <robux4@ycbcr.xyz>
2018-05-05 17:10:01 +0200

    input: make the seekpoint seeking tolerance explicit to 3s using CLOCK_FREQ

--- src/input/input.c
+++ src/input/input.c
@@ -2127,7 +2127,7 @@ static bool Control( input_thread_t *p_input,
                 int64_t i_input_time = var_GetInteger( p_input, "time" );
                 if( i_seekpoint_time >= 0 && i_input_time >= 0 )
                 {
-                    if( i_input_time < i_seekpoint_time + 3000000 )
+                    if( i_input_time < i_seekpoint_time + 3*CLOCK_FREQ )
                         i_seekpoint--;
                 }
                 else
