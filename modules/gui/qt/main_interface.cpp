0e120dd3b2fe9d09f9da09711476717375d2e09f
Pierre Lamot <pierre@videolabs.io>
2018-05-24 11:49:54 +0200

    WIP re-enable menu bar

--- modules/gui/qt/main_interface.cpp
+++ modules/gui/qt/main_interface.cpp
@@ -161,7 +161,11 @@ MainInterface::MainInterface( intf_thread_t *_p_intf ) : QVLCMW( _p_intf )
      **************************/
     setVLCWindowsTitle();
 
+    VLCMenuBar::createMenuBar( this, p_intf );
+
     createMainWidget( settings );
+
+
 
     /**************
      * Status Bar *
